f8adbe198490c084753034b20b7b1d8b3dfada68
Sangyu Lee <me@sangyul.ee>
2025-01-20 15:17:06 +0900

    add flake

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "saml2aws";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, flake-utils, nixpkgs }: flake-utils.lib.eachDefaultSystem
+    (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        packages = {
+          saml2aws = (pkgs.callPackage (import ./.) { });
+        };
+      }
+  );
+}
