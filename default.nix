0e9f034d1bbe03f4c73e483f88e1dc438542f275
Sangyu Lee <me@sangyul.ee>
2025-01-20 16:14:53 +0900

    fix errors on pure evaluation mode

--- default.nix
+++ default.nix
@@ -1,12 +1,20 @@
-{ buildGoModule, ... }:
+{ buildGoModule, stdenv, darwin, ... }:
 
-buildGoModule {
+buildGoModule rec {
   name = "saml2aws";
   version = "2.36.14-tencent";
   src = ./.;
+
+  buildInputs = [] ++ (if stdenv.hostPlatform.isDarwin then [
+    darwin.apple_sdk.frameworks.AppKit
+  ] else []);
+
+  ldflags = [
+    "-s"
+    "-w"
+    "-X main.Version=${version}"
+  ];
+
   vendorHash = "sha256-pml6M45IJXfeOiMcPq8K88LxQFR/WmOzQXwGXHGOCew";
-  env = {
-    CGO_ENABLED = "0";
-  };
   subPackages = [ "cmd/saml2aws" ];
 }
